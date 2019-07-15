7ffc314dba9d890d457c4e7f2d221aee181f43ab
Ronnie <16521174+beinnor@users.noreply.github.com>
2019-07-15 09:03:45 +0200

    Updates

---
+++ precache-manifest.136d8c8f20971fad71c730fb89a9a8a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab0637dbdd3ab413f49af6021c61823",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "7fa9dffdca3174db3dc8",
+    "url": "/pomodoro-timer/static/css/main.0c90f1fe.chunk.css"
+  },
+  {
+    "revision": "efc3d230fd1e1b3caa34",
+    "url": "/pomodoro-timer/static/js/2.bbcd3f81.chunk.js"
+  },
+  {
+    "revision": "7fa9dffdca3174db3dc8",
+    "url": "/pomodoro-timer/static/js/main.466f0fd4.chunk.js"
+  },
+  {
+    "revision": "8d84c672a99e0f0ece83",
+    "url": "/pomodoro-timer/static/js/runtime~main.2a0df6ba.js"
+  },
+  {
+    "revision": "1cd6ea462e87352e2083614a74abb90f",
+    "url": "/pomodoro-timer/static/media/alarm-sound.1cd6ea46.mp3"
+  },
+  {
+    "revision": "fed2ed0b639bf9b63c90fc8901bdea2e",
+    "url": "/pomodoro-timer/static/media/alarm-sound.fed2ed0b.ogg"
+  },
+  {
+    "revision": "dce4e0249e787c392fe388cb1f3e597d",
+    "url": "/pomodoro-timer/static/media/captions.dce4e024.vtt"
+  }
+]);
